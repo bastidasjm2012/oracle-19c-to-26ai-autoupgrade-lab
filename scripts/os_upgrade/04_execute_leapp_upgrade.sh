4979ccfbd6e47c131f22b12d307f4dd3178b6ad6
bastidasjm2012 <66046235+bastidasjm2012@users.noreply.github.com>
2026-06-02 18:21:25 -0500

    Create 04_execute_leapp_upgrade.sh

---
+++ scripts/os_upgrade/04_execute_leapp_upgrade.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+echo "================================"
+echo " Starting Oracle Linux Upgrade"
+echo " OL7.9 --> OL8.10"
+echo "================================"
+
+
+date
+
+
+leapp upgrade
+
+
+echo "
+If successful execute:
+
+reboot
+
+"
+
