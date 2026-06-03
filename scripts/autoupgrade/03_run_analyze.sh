3cd1e4cb4120c71ea84eae3e2536628d48c26afd
bastidasjm2012 <66046235+bastidasjm2012@users.noreply.github.com>
2026-06-02 21:58:58 -0500

    Create 03_run_analyze.sh

---
+++ scripts/autoupgrade/03_run_analyze.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+export ORACLE_HOME=/u01/app/oracle/product/26ai/dbhome_1
+
+export PATH=$ORACLE_HOME/bin:$ORACLE_HOME/jdk/bin:$PATH
+
+
+java \
+-jar /u01/app/oracle/autoupgrade/autoupgrade.jar \
+-config /u01/app/oracle/autoupgrade/autoupg.cfg \
+-mode analyze
