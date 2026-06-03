0d6e4da2ce47d646a3b3a345fb5de7a235e96e34
bastidasjm2012 <66046235+bastidasjm2012@users.noreply.github.com>
2026-06-02 21:56:42 -0500

    Update 01_prepare_autoupgrade.sh

--- scripts/autoupgrade/01_prepare_autoupgrade.sh
+++ scripts/autoupgrade/01_prepare_autoupgrade.sh
@@ -1,1 +1,32 @@
+#!/bin/bash
 
+AUTO_DIR=/u01/app/oracle/autoupgrade
+
+mkdir -p $AUTO_DIR
+
+
+echo "Copy AutoUpgrade jar"
+
+cp /u01/stage/26ai/autoupgrade.jar \
+$AUTO_DIR
+
+
+chmod 755 $AUTO_DIR
+
+
+echo "Validate Java"
+
+
+/u01/app/oracle/product/26ai/dbhome_1/jdk/bin/java \
+-version
+
+
+echo
+
+
+echo "Validate AutoUpgrade"
+
+
+/u01/app/oracle/product/26ai/dbhome_1/jdk/bin/java \
+-jar $AUTO_DIR/autoupgrade.jar \
+-version
