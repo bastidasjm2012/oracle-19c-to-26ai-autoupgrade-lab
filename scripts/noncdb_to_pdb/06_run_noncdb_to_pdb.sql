378bd7123d8a0169a4d2b9b311e43a36e797afcc
bastidasjm2012 <66046235+bastidasjm2012@users.noreply.github.com>
2026-06-10 10:18:37 -0500

    Update SQL script for noncdb to pdb process

--- scripts/noncdb_to_pdb/06_run_noncdb_to_pdb.sql
+++ scripts/noncdb_to_pdb/06_run_noncdb_to_pdb.sql
@@ -1,9 +1,8 @@
-set lines 220 pages 50000 trimspool on
-spool /u01/stage/26ai/noncdb_to_pdb/06_run_noncdb_to_pdb.log
+set lines 200
+set pages 200
+set timing on
 
-prompt ============================================================
-prompt RUN noncdb_to_pdb.sql
-prompt ============================================================
+spool /u01/evidence/phase05_noncdb_to_pdb/06_run_noncdb_to_pdb.log
 
 alter session set container=NEXUS;
 
@@ -11,13 +10,10 @@ show con_name
 
 @$ORACLE_HOME/rdbms/admin/noncdb_to_pdb.sql
 
-prompt ============================================================
-prompt CLOSE AND OPEN PDB READ WRITE
-prompt ============================================================
-
 alter pluggable database close immediate;
 alter pluggable database open;
 
 show pdbs
 
 spool off
+exit
